ca18f2fe81522540c393ae7f5a5723ab0a4bade4
kariyakarwana <kariyakarawanaz@gmail.com>
2026-01-16 08:41:03 +0530

    Dockerized the Backend

--- src/main/java/com/example/NVD_back/NvdBackApplication.java
+++ src/main/java/com/example/NVD_back/NvdBackApplication.java
@@ -15,13 +15,13 @@ public class NvdBackApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.load();
-
-		dotenv.entries().forEach(entry ->
-						System.setProperty(entry.getKey(), entry.getValue())
-		);
+//		Dotenv dotenv = Dotenv.configure()
+//				.ignoreIfMissing()
+//				.load();
+//
+//		dotenv.entries().forEach(entry ->
+//						System.setProperty(entry.getKey(), entry.getValue())
+//		);
 
 		SpringApplication.run(NvdBackApplication.class, args);
 	}
